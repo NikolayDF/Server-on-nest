0b34cacbb4ed00496f315c0cacbce2537213b716
Николай <78875139+NikolayDF@users.noreply.github.com>
2023-02-16 16:08:02 +0300

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Post, Get } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  
+  constructor(private usersService: UsersService) {
+    
+  }
+  
+  @Post()
+  create(@Body() userDto: CreateUserDto) {
+    return this.usersService.createUser(userDto);
+  }
+  
+  @Get()
+  getAll() {
+    return this.usersService.getAllUsers();
+  }
+
+}
