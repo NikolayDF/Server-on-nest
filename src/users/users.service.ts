8d40998be28c7f7d428e2fe4d63c07944fadd00f
Николай <78875139+NikolayDF@users.noreply.github.com>
2023-02-16 16:08:11 +0300

    Update users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InjectModel } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  
+  constructor(@InjectModel(User) private userRepository: typeof User) {
+    
+  }
+  
+  async createUser(dto.CreateUserDto) {
+    const user = await this.userRepository.create(dto);
+    return user;
+  }
+  
+  async getAllUsers() {
+    const users = await this.userRepository.findAll(dto);
+    return users;
+  }
+
+}
